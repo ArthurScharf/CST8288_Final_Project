6dd32a7f68c8a23f84be1b5b2d1b0325fb634ee2
BenGurth <gurt0001@algonquinlive.com>
2025-07-26 15:28:36 -0400

    NotificationDAO getAll
    getAll for notifciationDAO

--- src/main/java/dataaccesslayer/NotificationDAO.java
+++ src/main/java/dataaccesslayer/NotificationDAO.java
@@ -4,10 +4,40 @@
  */
 package dataaccesslayer;
 
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import transportobjects.NotificationDTO;
 /**
  *
  * @author gurth
  */
-public class NotificationDAO {
+public class NotificationDAO implements NotificationDAOInterface {
     
+    @Override
+    public ArrayList<NotificationDTO> getAll() throws SQLException {
+        String query = "SELECT * FROM notification";
+
+        Connection conn = DataSource.INSTANCE.getConnection();
+
+        ArrayList<NotificationDTO> notifications = new ArrayList<>();
+
+        try (Statement stmt = conn.createStatement(); ResultSet results = stmt.executeQuery(query)){
+            while (results.next()){
+                NotificationDTO dto = new NotificationDTO();
+                dto.setNotificationId(results.getInt("notification_id"));
+                dto.setVehicleId(results.getString("vehicle_id"));
+                dto.setNotificationType(results.getString("notification_type"));
+                dto.setData(results.getString("data"));
+                notifications.add(dto);
+            }
+        }
+        catch(SQLException e){
+            throw e;
+        }
+        return notifications;
+    }
 }
