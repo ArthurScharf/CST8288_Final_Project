aa703c2ae48f69bcbda49ed94a79c231cd40a85d
Sina-Paslar <sina83paslar@gmail.com>
2025-08-06 15:56:58 -0400

    Added the reports to table

--- src/main/java/businesslayer/report/Report.java
+++ src/main/java/businesslayer/report/Report.java
@@ -53,6 +53,7 @@ public class Report {
     
     
     
+    
             
     public Report(String locationTracking, int energyConsumption,
             List<String> transitMaintenance, HashMap<String, String> operatorPerformance,
@@ -126,7 +127,7 @@ public class Report {
     public List<String> toHTML()
     {
         
-               
+        float sum = 0;
         List<String> htmlElements = new ArrayList<>();
 
         if (locationTracking != null) {
@@ -230,86 +231,135 @@ public class Report {
 //
 //        }
         
-        if (busDTOList != null && !busDTOList.isEmpty()){
-           for (VehicleDTO vehicleDTO : vehicleDTOList){
-               if (vehicleDTO instanceof BusDTO busDTO){
-//                   String type = busDTO.serialize().substring(0, 3);
-//                   if (type.equalsIgnoreCase("Bus")){
-                       float remainFuel = busDTO.getResourceAmount() / busDTO.getMaxResource() * 100;
-                       float costToRefuel = 0;
-                        if (busDTO.getResourceType().equalsIgnoreCase("Diesel")){
-                            costToRefuel = remainFuel * DIESEL_PRICE / 100;
-                        } else if (busDTO.getResourceType().equalsIgnoreCase("CNG") ){
-                            costToRefuel = remainFuel * CNG_PRICE / 100;
-                        }
+        if (busDTOList != null && !busDTOList.isEmpty()) {
+            StringBuilder busTable = new StringBuilder();
+            float totalBusCost = 0;
 
-                        CurrecnyStrategyContext cuurrencyContext = new CurrecnyStrategyContext(mode); 
-                        costToRefuel  = (float) cuurrencyContext.currencyConvertor(costToRefuel);
+            busTable.append("<table border='1' cellpadding='5' cellspacing='0'>");
+            busTable.append("<tr><th colspan='4'>Bus Report</th></tr>");
+            busTable.append("<tr>")
+                    .append("<th>Bus Number</th>")
+                    .append("<th>Bus Type</th>")
+                    .append("<th>Remaining Fuel (%)</th>")
+                    .append("<th>Cost to Refuel ($)</th>")
+                    .append("</tr>");
 
-                         htmlElements.add("<p><strong>Bus Number:</strong> " + busDTO.getVehicleNumber() + " &nbsp; | &nbsp; " +
-                       "<strong>Bus Type:</strong> " + busDTO.getResourceType() + " &nbsp; | &nbsp; " +
-                       "<strong>Bus Remaining Fuel:</strong> " + remainFuel + "% &nbsp; | &nbsp; " +
-                       "<strong>Cost to Refuel:</strong> " + costToRefuel + "$</p>");
+            for (VehicleDTO vehicleDTO : vehicleDTOList) {
+                if (vehicleDTO instanceof BusDTO busDTO) {
+                    float remainFuel = busDTO.getResourceAmount() / busDTO.getMaxResource() * 100;
+                    float costToRefuel = 0;
 
-                     } else {
-                         System.out.println("\nOOOPS\n");
-                     }
-                       
-//                   }
-                  
-           }
+                    if (busDTO.getResourceType().equalsIgnoreCase("Diesel")) {
+                        costToRefuel = remainFuel * DIESEL_PRICE / 100;
+                    } else if (busDTO.getResourceType().equalsIgnoreCase("CNG")) {
+                        costToRefuel = remainFuel * CNG_PRICE / 100;
+                    }
+
+                    CurrecnyStrategyContext currencyContext = new CurrecnyStrategyContext(mode);
+                    costToRefuel = (float) currencyContext.currencyConvertor(costToRefuel);
+                    totalBusCost += costToRefuel;
+
+                    busTable.append("<tr>")
+                            .append("<td>").append(busDTO.getVehicleNumber()).append("</td>")
+                            .append("<td>").append(busDTO.getResourceType()).append("</td>")
+                            .append("<td>").append(String.format("%.2f", remainFuel)).append("%</td>")
+                            .append("<td>").append(String.format("%.2f", costToRefuel)).append("</td>")
+                            .append("</tr>");
+                }
         }
+
+    busTable.append("<tr><td colspan='3'><strong>Total Refuel Cost</strong></td>")
+            .append("<td><strong>").append(String.format("%.2f", totalBusCost)).append("</strong></td></tr>");
+
+    busTable.append("</table><br>");
+    htmlElements.add(busTable.toString());
+}
         
-        if (deiselElectricDTOList != null && !deiselElectricDTOList.isEmpty()){
-            for (VehicleDTO vehicleDTO : vehicleDTOList){
-               if (vehicleDTO instanceof DieselElectricDTO dieselElectricDTO){
-                  
-                float remainBattery = (dieselElectricDTO.getBatteryAmount() / dieselElectricDTO.getMaxBattery()) * 100;
-                float remainFuel = (dieselElectricDTO.getFuelAmount() / dieselElectricDTO.getMaxFuel()) * 100;
 
-                
-                float costToRefuel = (remainFuel * DIESEL_PRICE + remainBattery * BATTERY_PRICE) / 100; 
-                
-                
-                   CurrecnyStrategyContext cuurrencyContext = new CurrecnyStrategyContext(mode); 
-                   costToRefuel  = (float) cuurrencyContext.currencyConvertor(costToRefuel);
 
-                htmlElements.add("<p><strong>Diesel-Electric Number:</strong> " + dieselElectricDTO.getVehicleNumber() + " &nbsp; | &nbsp; " +
-                                 "<strong>Battery Remaining:</strong> " + String.format("%.2f", remainBattery) + "% &nbsp; | &nbsp; " +
-                                 "<strong>Fuel Remaining:</strong> " + String.format("%.2f", remainFuel) + "% &nbsp; | &nbsp; " +
-                                 "<strong>Cost to Refuel:</strong> " + String.format("%.2f", costToRefuel) + "$</p>");
-               } else {
-                   System.out.println("\nOOOPS\n");
-               }
-           }
+        if (deiselElectricDTOList != null && !deiselElectricDTOList.isEmpty()) {
+            StringBuilder tableBuilder = new StringBuilder();
+            sum = 0;
+
+            // Start single table
+            tableBuilder.append("<table border='1' cellpadding='5' cellspacing='0'>");
+            tableBuilder.append("<tr><th colspan='4'>Diesel-Electric Train Report</th></tr>");
+            tableBuilder.append("<tr>")
+                        .append("<th>Diesel-Electric Number</th>")
+                        .append("<th>Battery Remaining (%)</th>")
+                        .append("<th>Fuel Remaining (%)</th>")
+                        .append("<th>Cost to Refuel ($)</th>")
+                        .append("</tr>");
+
+            for (VehicleDTO vehicleDTO : vehicleDTOList) {
+                if (vehicleDTO instanceof DieselElectricDTO dieselElectricDTO) {
+                    float remainBattery = (dieselElectricDTO.getBatteryAmount() / dieselElectricDTO.getMaxBattery()) * 100;
+                    float remainFuel = (dieselElectricDTO.getFuelAmount() / dieselElectricDTO.getMaxFuel()) * 100;
+
+                    float costToRefuel = (remainFuel * DIESEL_PRICE + remainBattery * BATTERY_PRICE) / 100;
+                    sum += costToRefuel;
+
+                    CurrecnyStrategyContext currencyContext = new CurrecnyStrategyContext(mode);
+                    costToRefuel = (float) currencyContext.currencyConvertor(costToRefuel);
+
+                    tableBuilder.append("<tr>")
+                                .append("<td>").append(dieselElectricDTO.getVehicleNumber()).append("</td>")
+                                .append("<td>").append(String.format("%.2f", remainBattery)).append("%</td>")
+                                .append("<td>").append(String.format("%.2f", remainFuel)).append("%</td>")
+                                .append("<td>").append(String.format("%.2f", costToRefuel)).append("</td>")
+                                .append("</tr>");
+                } else {
+                    System.out.println("\nOOOPS\n");
+                }
+            }
+
+            // Total row
+            tableBuilder.append("<tr><td colspan='3'><strong>Total Refuel Cost</strong></td>")
+                        .append("<td><strong>").append(String.format("%.2f", sum)).append("</strong></td></tr>");
+
+            tableBuilder.append("</table><br>");
+            htmlElements.add(tableBuilder.toString());
         }
+
+
+
+    if (lightRailDTO != null && !lightRailDTO.isEmpty()) {
+        StringBuilder lightRailTable = new StringBuilder();
+        float totalRechargeCost = 0;
+
+        lightRailTable.append("<table border='1' cellpadding='5' cellspacing='0'>");
+        lightRailTable.append("<tr><th colspan='3'>Light Rail Report</th></tr>");
+        lightRailTable.append("<tr>")
+                      .append("<th>Light Rail Number</th>")
+                      .append("<th>Battery Remaining (%)</th>")
+                      .append("<th>Cost to Recharge ($)</th>")
+                      .append("</tr>");
+
+        for (VehicleDTO vehicleDTO : vehicleDTOList) {
+            if (vehicleDTO instanceof LightRailDTO lightDTO) {
+                float remainBattery = lightDTO.getBatteryAmount() / lightDTO.getMaxBattery() * 100;
+                float costToRecharge = (lightDTO.getMaxBattery() - lightDTO.getBatteryAmount()) * BATTERY_PRICE / 100;
+
+                CurrecnyStrategyContext currencyContext = new CurrecnyStrategyContext(mode);
+                costToRecharge = (float) currencyContext.currencyConvertor(costToRecharge);
+                totalRechargeCost += costToRecharge;
+
+                lightRailTable.append("<tr>")
+                              .append("<td>").append(lightDTO.getVehicleNumber()).append("</td>")
+                              .append("<td>").append(String.format("%.2f", remainBattery)).append("%</td>")
+                              .append("<td>").append(String.format("%.2f", costToRecharge)).append("</td>")
+                              .append("</tr>");
+            }
+        }
+
+        lightRailTable.append("<tr><td colspan='2'><strong>Total Recharge Cost</strong></td>")
+                      .append("<td><strong>").append(String.format("%.2f", totalRechargeCost)).append("</strong></td></tr>");
+
+        lightRailTable.append("</table><br>");
+        htmlElements.add(lightRailTable.toString());
+    }
         
-        if (lightRailDTO != null && !lightRailDTO.isEmpty()){
-            //if mode is sth do usd to cad else do otherwise
-            
-            
-            
-            
-            for (VehicleDTO vehicleDTO : vehicleDTOList){
-               if (vehicleDTO instanceof LightRailDTO lightRailDTO){
-                   float remainBatt = ( lightRailDTO.getBatteryAmount() / lightRailDTO.getMaxBattery()) * 100;
-                   float costToRecharge = ((lightRailDTO.getMaxBattery() - lightRailDTO.getBatteryAmount()) * BATTERY_PRICE) / 100;
-                   
-                   
-                   
-                   CurrecnyStrategyContext cuurrencyContext = new CurrecnyStrategyContext(mode); 
-                   costToRecharge  = (float) cuurrencyContext.currencyConvertor(costToRecharge);
-                   
-                   htmlElements.add("<p><strong>Light Rail Number:</strong> " + lightRailDTO.getVehicleNumber() + " &nbsp; | &nbsp; " +
-                 "<strong>Remaining Battery:</strong> " + remainBatt + "% &nbsp; | &nbsp; " +
-                 "<strong>Cost to recharge:</strong> " + costToRecharge + "</p>");
-
-               } else {
-                   System.out.println("\nOOOPS\n");
-               }
-           }
-        }
-
+        
         return htmlElements;
 
     }
